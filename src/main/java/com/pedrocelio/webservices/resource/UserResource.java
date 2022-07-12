fb6923b42a22c3e8554818ab0e67ac17c7940e1b
Pedro Célio <pedroceliob3@gmail.com>
2022-07-12 19:53:24 -0300

    Usando h2 database

--- src/main/java/com/pedrocelio/webservices/resource/UserResource.java
+++ src/main/java/com/pedrocelio/webservices/resource/UserResource.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pedrocelio.webservices.entities.User;
+import com.pedrocelio.webservices.entities.Tb_User;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Pedro", "pedro@gmail.com", "124521522", "12345678");
+	public ResponseEntity<Tb_User> findAll() {
+		Tb_User u = new Tb_User(1L, "Pedro", "pedro@gmail.com", "124521522", "12345678");
 		return ResponseEntity.ok().body(u);
 	}
 }
